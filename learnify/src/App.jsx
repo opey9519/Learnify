a14ce79ff2a9fbbd0d7a983d9fae88319d76a83a
Gavin Wilson <gmanwilson@icloud.com>
2025-01-19 16:06:04 -0500

    Created Footer Component

--- learnify/src/App.jsx
+++ learnify/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css"
 import Header from "./Header"
 import WelcomeHome from "./WelcomeHome"
+import Footer from "./Footer"
 
 function App() {
   return (
     <div>
       <Header />
       <WelcomeHome />
+      <Footer />
     </div>
   )
 }
