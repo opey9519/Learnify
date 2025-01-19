51996a0767d2e219babf0b06f4ec9e92928cffda
Gavin Wilson <gmanwilson@icloud.com>
2025-01-19 16:02:27 -0500

    Improved Components and styles for Home Component

--- learnify/src/WelcomeHome.jsx
+++ learnify/src/WelcomeHome.jsx
@@ -3,8 +3,8 @@ import "./WelcomeHome.css"
 function WelcomeHome() {
     return (
         <div id="root" className="container root">
-            <div className="text-primary-200">
-                <h1>Welcome Home!</h1>
+            <div className="text">
+                <h1 id="title">Welcome Home!</h1>
                 <p>Welcome to Learnify – your ultimate study companion! Easily create, organize, and review flashcards to master any topic. Whether you're preparing for exams, learning new skills, or just exploring new ideas, we've got you covered. Ready to boost your learning? Let’s get started!</p>
             </div>
             <div className="selectButton">
