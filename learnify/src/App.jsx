3419751ecece3173646bdbc49aa3b96a404794af
Gavin Wilson <gmanwilson@icloud.com>
2025-01-24 16:50:58 -0500

    Made About file page and linked it in App jsx

--- learnify/src/App.jsx
+++ learnify/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import "./App.css"
 import Home from "./pages/Home"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import About from "./pages/About"
 
 
 
@@ -10,6 +11,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </Router>
   )
