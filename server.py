f2538a457d793d1e37e3a03f2e9f6d3c93aced88
Gavin Wilson <gmanwilson@icloud.com>
2025-01-31 23:47:51 -0500

    New User route with python object

--- server.py
+++ server.py
@@ -1,5 +1,8 @@
 # Dependencies
-from flask import Flask, request, jsonifyx
+from flask import Flask, request, jsonify
 
+app = Flask(__name__)
+
+@app.route('/newuser')
 def get_new_user():
     pass
