d323088300771fd65e54c95d16833dc748b9f015
Gavin Wilson <gmanwilson@icloud.com>
2025-02-01 19:35:39 -0500

    Imported dependencies to enable CORS activity and created first API to get all flashcards with fake data

--- server.py
+++ server.py
@@ -1,8 +1,27 @@
 # Dependencies
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
+# Fake Database
+flashcards = [
+    {
+        "question": "What is the fastest land animal?",
+        "answer": "Cheetah"
+    },
+    {
+        "question": "What is the tallest bird?",
+        "answer": "North African Ostrich" 
+    }
+]
+
+# Create instance of Flask
 app = Flask(__name__)
+CORS(app)  # Allows Flask API to handle requests to React App
 
-@app.route('/newuser')
-def get_new_user():
-    pass
+# API route to get all flashcards 
+@app.route("/api/flashcards", methods =["GET"])
+def get_flashcards():
+    return jsonify(flashcards)
+
+if __name__ == "__main__":
+    app.run()
