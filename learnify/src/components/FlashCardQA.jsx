209b6bab104a0366057f601ff960ff9b06f1ddcc
Gavin Wilson <gmanwilson@icloud.com>
2025-02-03 09:35:01 -0500

    Additional documentation and cleaner code

--- learnify/src/components/FlashCardQA.jsx
+++ learnify/src/components/FlashCardQA.jsx
@@ -5,9 +5,10 @@ import { useLocation } from "react-router-dom";
 // Display FlashCard Question & Answer
 function FlashCardQA() {
     // Retrieve passed data
-    const { state } = useLocation();
+    const { state } = useLocation(); // Destructure state from location 
     const flashcardSet = state.flashcardSet;
 
+    // If flashcard set data is not found - display message
     if (!flashcardSet) {
         return <p>No FlashCards Found!</p>
     }
@@ -20,11 +21,14 @@ function FlashCardQA() {
 
     return (
         <div className="container FlashCardQA">
+            {/* Flashcard flipped - Apply necesssary styles */}
             <div className={`flashcard ${isFlipped ? "flipped" : ""}`} onClick={handleFlip}>
+                {/* Need to find a way to not hardcode the index */}
                 <div className="front">{flashcardSet.set_info.cards[0].question}</div>
                 <div className="back">{flashcardSet.set_info.cards[0].answer}</div>
             </div>
 
+            {/* Controls should navigate from one flashcard to another*/}
             <div className="controls">
                 <button>Next</button>
                 <button>Prev</button>
--- learnify/src/components/FlashCardSet.jsx
+++ learnify/src/components/FlashCardSet.jsx
@@ -6,34 +6,36 @@ import { fetchFlashcards } from "../api";
 
 // Displays Individual Flashcard Sets
 function FlashCardSet() {
+
     // State to dynamically display flashcard sets from backend
     const [flashcardSets, setFlashcardSets] = useState([])
-    // Async function to deal with awaiting flashcard data from backend API
-    useEffect(() => {
+    useEffect(() => { // Async function to deal with awaiting flashcard data from backend API
         async function loadData() {
-            const data = await fetchFlashcards();
-            console.log(data)
-            setFlashcardSets(Array.isArray(data) ? data : [data]);
+            const data = await fetchFlashcards(); // waiting for fetchflashcards to pull backend data
+            setFlashcardSets(Array.isArray(data) ? data : [data]); // Check if data is array, if not wrap data in array for mapping
         }
         loadData();
     }, [])
 
+
     const navigate = useNavigate();
-    const handleClick = (setObj) => {
+    const handleClick = (setObj) => { // On click, navigate inside flashcard set with data passed through state object
         navigate("/flashcard-set", { state: { flashcardSet: setObj } });
     }
 
+    
     return (
         <div className="container Set">
+            {/* If flashcard set(s) exist, generate from backend data */}
             {flashcardSets.length > 0 ? (
-                flashcardSets.map((setObj, index) => (
+                flashcardSets.map((setObj, index) => ( // Create new array of divs looping over backend data
                     <div onClick={() => handleClick(setObj)} key={index} className="FlashCardSet">
                         <h3>{setObj.set_info.set_title || "Untitled Set"}</h3> {/* Data from API */}
-                        <FlashCardUserCards user={setObj.set_info.user} numCards={setObj.set_info.num_cards} />
+                        <FlashCardUserCards user={setObj.set_info.user} numCards={setObj.set_info.num_cards} /> {/* Component to show user & num_cards */}
                     </div>
                 ))
             ) : (
-                <p>Loading Flashcards...</p>
+                <p>Loading Flashcards...</p> // If no data...
             )
             }
         </div>
