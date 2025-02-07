b73a49520e081663726c5aa487c6a2c6c21c05f9
Gavin Wilson <gmanwilson@icloud.com>
2025-02-07 10:17:32 -0500

    Improved and worked on navbar dropdown

--- learnify/src/components/Header.jsx
+++ learnify/src/components/Header.jsx
@@ -1,20 +1,37 @@
+import { useState } from "react";
 import "./Header.css"
 
 function Header() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleNavbar = () => {
+        setIsOpen(!isOpen);
+    }
+
     return (
         <nav id="nav" className="navbar">
             <div className="container">
                 <div>
                     <a id="home" className="tag" href="/">Learnify</a>
                 </div>
-                <div className="tagsContainer">
-                    <a className="tag hide" href="">My Flashcards</a>
-                    <a className="tag hide" href="">AI Assistance</a>
-                    <button id="dropdown" className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span id="dropdownIcon" className="navbar-toggler-icon"></span>
+                <div className="tagsContainer navbarContainer">
+                    <div className={`nav-links ${isOpen ? "show" : ""}`}>
+                        <a className="tag hide" href="">My Flashcards</a>
+                        <a className="tag hide" href="">AI Assistance</a>
+                    </div>
+
+                    <button onClick={toggleNavbar} id="dropdown" className="navbarToggler" type="button">
+                        <span className="navbar-toggler-icon" id="dropdownIcon"></span>
+                        
+                        {/* <span id="dropdownIcon">≡</span> */}
+                        
                     </button>
+                    
                 </div>
+                
             </div>
+            {isOpen ? <a className="tag" href="">My Flashcards</a> : <></>}
+            {isOpen ? <a className="tag" href="">AI Assistance</a> : <></>}
         </nav>
     );
 }
