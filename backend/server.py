7dca0cec622011d73b92a87cb30f7baf1d680d1b
Gavin Wilson <gmanwilson@icloud.com>
2025-02-10 20:48:48 -0500

    Installed and linked PostgreSQl and SQLAlchemy in backend

--- backend/server.py
+++ backend/server.py
@@ -1,6 +1,8 @@
 # Dependencies
 from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+
 
 flashcards = {
     "set_info": {
@@ -26,9 +28,24 @@ flashcards = {
 app = Flask(__name__)
 CORS(app)  # Allows Flask API to handle requests to React App
 
+# Configuring Flask to PostgreSQL
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://learnify_user:securepassword@localhost/learnify_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app) # Creating SQLAlchemy Object
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+
+    def __repr__(self):
+        return f"<User {self.username}>"
+
+
 # API route to get all flashcards
-
-
 @app.route("/api/flashcards", methods=["GET"])
 def get_flashcards():
     return jsonify(flashcards)
