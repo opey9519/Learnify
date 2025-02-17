21760a690c5d6841235c8a8c5fb28a530205be58
Gavin Wilson <gmanwilson@icloud.com>
2025-02-16 21:55:11 -0500

    Created LoginForm skeleton w/ basic styling

--- learnify/src/components/LoginForm.jsx
+++ learnify/src/components/LoginForm.jsx
@@ -1,8 +1,34 @@
+import "./LoginForm.css"
+
 function LoginForm() {
     return (
-        <div>
-            <form action="
-            "></form>
+        <div className="container LoginForm">
+            <form className="Form" action="" method="post">
+                <div className="Credentials">
+                    <div className="UserBox">
+                        <label className="Username" htmlFor="">Username</label>
+                        <input className="Username" type="text" />
+                    </div>
+
+                    <div className="PassBox">
+                        <label className="Password" htmlFor="">Password</label>
+                        <input className="Password" type="password" />
+                    </div>
+                    {/* Need to create Email specific box */}
+                    <div className="EmailBox">
+                        <label className="Email" htmlFor="">Email</label>
+                        <input className="Email" type="email" />
+                    </div>
+
+                </div>
+
+                <div className="SubmitButton">
+                    <button className="Login">
+                        Login
+                    </button>
+                </div>
+
+            </form>
         </div>
     );
 }
--- learnify/src/pages/Login.jsx
+++ learnify/src/pages/Login.jsx
@@ -1,10 +1,12 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import LoginForm from "../components/LoginForm";
 
 function Login() {
     return (
         <div>
             <Header />
+            <LoginForm />
             <Footer />
         </div>
     );
