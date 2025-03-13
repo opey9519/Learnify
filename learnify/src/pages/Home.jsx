aff6268d8cebadd70879c88a3f94ea9066d9cc17
Gavin Wilson <gmanwilson@icloud.com>
2025-03-13 16:10:22 -0400

    Fixed bug- flashcards not showing due to local data

--- learnify/src/pages/Home.jsx
+++ learnify/src/pages/Home.jsx
@@ -3,17 +3,12 @@ import WelcomeHome from "../components/WelcomeHome";
 import Footer from "../components/Footer";
 import FlashCardList from "../components/FlashCardList";
 
-const tempFlashCard = {
-    question: "What Animal lives in Africa?",
-    answer: "Giraffe"
-}
-
 function Home() {
     return (
         <div>
             <Header />
             <WelcomeHome />
-            <FlashCardList question={tempFlashCard.question} answer={tempFlashCard.answer} />
+            <FlashCardList />
             <Footer />
         </div>
     );
