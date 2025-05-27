51bca39f5ed972bce28f2e37570f943814ddff91
Gavin Wilson <gmanwilson@icloud.com>
2025-05-26 22:17:53 -0400

    Added OpenAI API env variable

--- backend/config.py
+++ backend/config.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 from urllib.parse import quote
 
-load_dotenv() # Loads environment variables from external file
+load_dotenv()  # Loads environment variables from external file
 
 # Database credentials with Environment Variables
 DB_USER = os.getenv("DB_USER", "postgres")
@@ -13,7 +13,10 @@ DB_NAME = os.getenv("DB_NAME", "learnify_db")
 
 # Connection string SQLAlchemy uses to connect to PostgreSQL
 SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-SQLALCHEMY_TRACK_MODIFICATIONS = False # Disables Flask-SQLAlchemy event tracking for performance
+# Disables Flask-SQLAlchemy event tracking for performance
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-# JWT Enviornment Variables
+# JWT Enviornment Variables - Set this in .env
 JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+# OpenAI API Key - Generate this from OpenAPI (https://openai.com/api/) and set in .env
+OPEN_AI_KEY = os.getenv("OPEN_AI_KEY")
