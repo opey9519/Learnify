b6a9ab1c9c6ebaf40a2cb5965c613229940bc7f8
Gavin Wilson <gmanwilson@icloud.com>
2025-06-17 10:16:44 -0400

    Removed Start Studying button

--- learnify/src/components/WelcomeHome.jsx
+++ learnify/src/components/WelcomeHome.jsx
@@ -9,7 +9,7 @@ function WelcomeHome() {
                 <p>Welcome to Learnify – your ultimate study companion! Easily create, organize, and review flashcards to master any topic. Whether you're preparing for exams, learning new skills, or just exploring new ideas, we've got you covered. Ready to boost your learning? Let’s get started!</p>
             </div>
             <div className="selectButton">
-                <button id="studyButton" className="btn">Start Studying</button>
+                {/* <button id="studyButton" className="btn">Start Studying</button> */}
                 {/* <button id="cardButton" className="btn">Create Flashcard Sets</button> */}
                 <Link id="cardButton" className="btn" to="/create-flashcard-set">Create Flashcard Sets</Link>
             </div>
