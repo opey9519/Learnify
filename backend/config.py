b8581dca43811051150d528de76e414885d704a2
Gavin Wilson <gmanwilson@icloud.com>
2025-07-18 09:58:11 -0400

    Updated config to support Render database credentials

--- backend/config.py
+++ backend/config.py
@@ -11,9 +11,11 @@ DB_PASSWORD = quote(os.getenv("DB_PASSWORD"))
 DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT", "5432")
 DB_NAME = os.getenv("DB_NAME", "learnify_db")
+RENDER_EXTERNAL_URI = os.getenv("DATABASE_URI")
 
 # Connection string SQLAlchemy uses to connect to PostgreSQL
-SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+# SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+SQLALCHEMY_DATABASE_URI = RENDER_EXTERNAL_URI
 # Disables Flask-SQLAlchemy event tracking for performance
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
