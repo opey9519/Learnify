d9c3d28be5646072060daa84af9bdb22f9e68c93
Gavin Wilson <gmanwilson@icloud.com>
2025-07-18 10:36:55 -0400

    Create tables test

--- backend/server.py
+++ backend/server.py
@@ -556,8 +556,11 @@ def generateFlashcards():
         return jsonify({"message": "AI generation failed", "error": str(e)}), 500
 
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    with app.app_context():
-        db.create_all()
-    app.run(host="0.0.0.0", port=port)
+# if __name__ == "__main__":
+#     port = int(os.environ.get("PORT", 5000))
+#     with app.app_context():
+#         db.create_all()
+#     app.run(host="0.0.0.0", port=port)
+
+with app.app_context():
+    db.create_all()
